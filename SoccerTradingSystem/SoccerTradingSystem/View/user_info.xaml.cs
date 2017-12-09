fc71d768831a6a1eb701a574d4da7c193b6865e9
DESKTOP-0OUJ9EB\fkrlsp2 <fkrlsp2@naver.com>
2017-12-09 12:03:10 +0900

    add func change auth

--- SoccerTradingSystem/SoccerTradingSystem/View/user_info.xaml.cs
+++ SoccerTradingSystem/SoccerTradingSystem/View/user_info.xaml.cs
@@ -18,6 +18,7 @@ using Player = SoccerTradingSystem.Model.Player;
 using Club = SoccerTradingSystem.Model.Club;
 using Manager = SoccerTradingSystem.Model.Manager;
 using User = SoccerTradingSystem.Model.User;
+using RetrieveHandler = SoccerTradingSystem.Controller.RetrieveHandler;
 using SystemAccountHandler = SoccerTradingSystem.Controller.SystemAccountHandler;
 
 namespace SoccerTradingSystem.Views
@@ -25,6 +26,10 @@ namespace SoccerTradingSystem.Views
     /// <summary>
     /// user_info.xaml에 대한 상호 작용 논리
     /// </summary>
+    /// 
+
+    using JSON = List<Dictionary<string, object>>;
+
     public partial class user_info : Page
     {
         bool flag = false;
@@ -47,22 +52,34 @@ namespace SoccerTradingSystem.Views
         private void Player_Row_DoubleClick(object sender, MouseButtonEventArgs e)
         {
             SystemAccountHandler sah = new SystemAccountHandler();
+            RetrieveHandler rh = new RetrieveHandler();
             DataRowView row = (DataRowView)playerDataGrid.SelectedItems[0];
             int uid = Convert.ToInt32((row[0]));
-            //bool auth = sah.retrieveUserData(uid).authenticated;
-            //if(auth)
-            //{
-            //    if (MessageBox.Show("현재 유저는 관리자 인증이 활성화 된 상태입니다.\n인증을 비활성화 하시겠습니까?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
-            //    {
-            //        sah.updateUserAuth(uid, false);
-            //    }
-            //}else
-            //{
-            //    if (MessageBox.Show("현재 유저는 관리자 인증이 비활성화 된 상태입니다.\n인증을 활성화 하시겠습니까?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
-            //    {
-            //        sah.updateUserAuth(uid, true);
-            //    }
-            //}
+
+            JSON filter = new JSON();
+            filter.Add(new Dictionary<string, object>());
+            filter[0].Add("uid", uid);
+            List<User> users = rh.retrieveUser(filter);
+            rh.retrievePlayer(null);
+
+            bool auth = users[0].authenticated;
+
+            if (auth)
+            {
+                if (MessageBox.Show("현재 유저는 관리자 인증이 활성화 된 상태입니다.\n인증을 비활성화 하시겠습니까?", "question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    User _user = new User(users[0].uid, users[0].email, users[0].password, false);
+                    sah.updateUser(_user);
+                }
+            }
+            else
+            {
+                if (MessageBox.Show("현재 유저는 관리자 인증이 비활성화 된 상태입니다.\n인증을 활성화 하시겠습니까?", "question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    User _user = new User(users[0].uid, users[0].email, users[0].password, true);
+                    sah.updateUser(_user);
+                }
+            }
             PlayersDataGridSetting("");
         }
 
@@ -72,6 +89,7 @@ namespace SoccerTradingSystem.Views
             SystemAccountHandler sah = new SystemAccountHandler();
             DataRowView row = (DataRowView)clubDataGrid.SelectedItems[0];
             int uid = Convert.ToInt32((row[0]));
+
             //bool auth = sah.retrieveUserData(uid).authenticated;
             //if (auth)
             //{
@@ -169,42 +187,47 @@ namespace SoccerTradingSystem.Views
         // 플레이어 그리드 구성
         public void PlayersDataGridSetting(string context)
         {
-            //SystemAccountHandler sah = new SystemAccountHandler();
-            //List<Player> list = sah.retrievePlayerData(context);
+            SystemAccountHandler sah = new SystemAccountHandler();
+            RetrieveHandler rh = new RetrieveHandler();
 
-            //// DataTable 생성
-            //DataTable dataTable = new DataTable();
+            JSON filter = new JSON();
+            filter.Add(new Dictionary<string, object>());
+            List<User> users = rh.retrieveUser(filter);
+            List<Player> list = rh.retrievePlayer(null);
 
-            //// 컬럼 생성
-            //dataTable.Columns.Add("uid", typeof(string));
-            //dataTable.Columns.Add("pid", typeof(string));
-            //dataTable.Columns.Add("email", typeof(string));
-            //dataTable.Columns.Add("name", typeof(string));
-            //dataTable.Columns.Add("birth", typeof(string));
-            //dataTable.Columns.Add("position", typeof(string));
-            //dataTable.Columns.Add("weight", typeof(string));
-            //dataTable.Columns.Add("height", typeof(string));
-            //dataTable.Columns.Add("status", typeof(string));
-            //dataTable.Columns.Add("authenticated", typeof(string));
+            // DataTable 생성
+            DataTable dataTable = new DataTable();
 
-            //// 데이터 생성
-            //for (int i = 0; i < list.Count; i++)
-            //{
-            //    string uid = Convert.ToString(list[i].uid);
-            //    string pid = Convert.ToString(list[i].playerId);
-            //    string email = list[i].email;
-            //    string name = list[i].firstName + list[i].middleName + list[i].lastName;
-            //    string birth = Convert.ToString(list[i].birth);
-            //    string postion = Convert.ToString(list[i].position);
-            //    string weight = Convert.ToString(list[i].weight);
-            //    string height = Convert.ToString(list[i].height);
-            //    string status = list[i].status;
-            //    string authenticated = (list[i].authenticated) ? "TRUE" : "FALSE";
-            //    dataTable.Rows.Add(new string[] { uid, pid, email, name, birth, postion, weight, height, status, authenticated });
-            //}
+            // 컬럼 생성
+            dataTable.Columns.Add("uid", typeof(string));
+            dataTable.Columns.Add("pid", typeof(string));
+            dataTable.Columns.Add("email", typeof(string));
+            dataTable.Columns.Add("name", typeof(string));
+            dataTable.Columns.Add("birth", typeof(string));
+            dataTable.Columns.Add("position", typeof(string));
+            dataTable.Columns.Add("weight", typeof(string));
+            dataTable.Columns.Add("height", typeof(string));
+            dataTable.Columns.Add("status", typeof(string));
+            dataTable.Columns.Add("authenticated", typeof(string));
 
-            //// DataTable의 Default View를 바인딩하기
-            //playerDataGrid.ItemsSource = dataTable.DefaultView;
+            // 데이터 생성
+            for (int i = 0; i < list.Count; i++)
+            {
+                string uid = Convert.ToString(list[i].uid);
+                string pid = Convert.ToString(list[i].playerId);
+                string email = list[i].email;
+                string name = list[i].firstName + list[i].middleName + list[i].lastName;
+                string birth = Convert.ToString(list[i].birth);
+                string postion = Convert.ToString(list[i].position);
+                string weight = Convert.ToString(list[i].weight);
+                string height = Convert.ToString(list[i].height);
+                string status = list[i].status;
+                string authenticated = (list[i].authenticated) ? "TRUE" : "FALSE";
+                dataTable.Rows.Add(new string[] { uid, pid, email, name, birth, postion, weight, height, status, authenticated });
+            }
+
+            // DataTable의 Default View를 바인딩하기
+            playerDataGrid.ItemsSource = dataTable.DefaultView;
         }
 
         // 클럽 그리드 구성
@@ -278,32 +301,32 @@ namespace SoccerTradingSystem.Views
 
         private void UserDataGridSetting(string context)
         {
-           // SystemAccountHandler sah = new SystemAccountHandler();
-           // List<User> ulist = sah.retrieveUserData(context);
+            // SystemAccountHandler sah = new SystemAccountHandler();
+            // List<User> ulist = sah.retrieveUserData(context);
 
-           // // DataTable 생성
-           // DataTable dataTable = new DataTable();
+            // // DataTable 생성
+            // DataTable dataTable = new DataTable();
 
-           // // 컬럼 생성
-           // dataTable.Columns.Add("uid", typeof(string));
-           // dataTable.Columns.Add("email", typeof(string));
-           // dataTable.Columns.Add("password", typeof(string));
-           // dataTable.Columns.Add("logined", typeof(string));
-           // dataTable.Columns.Add("authenticated", typeof(string));
+            // // 컬럼 생성
+            // dataTable.Columns.Add("uid", typeof(string));
+            // dataTable.Columns.Add("email", typeof(string));
+            // dataTable.Columns.Add("password", typeof(string));
+            // dataTable.Columns.Add("logined", typeof(string));
+            // dataTable.Columns.Add("authenticated", typeof(string));
 
-           // // 데이터 생성
-           // for (int i = 0; i < ulist.Count; i++)
-           // {
-           //     string uid = Convert.ToString(ulist[i].uid);
-           //     string email = Convert.ToString(ulist[i].email);
-           //     string password = Convert.ToString(ulist[i].password);
-           //     string logined = Convert.ToString(ulist[i].logined);
-           //     string authenticated = (ulist[i].authenticated) ? "TRUE" : "FALSE";
-           //     dataTable.Rows.Add(new string[] { uid, email, password, logined, authenticated });
-           // }
+            // // 데이터 생성
+            // for (int i = 0; i < ulist.Count; i++)
+            // {
+            //     string uid = Convert.ToString(ulist[i].uid);
+            //     string email = Convert.ToString(ulist[i].email);
+            //     string password = Convert.ToString(ulist[i].password);
+            //     string logined = Convert.ToString(ulist[i].logined);
+            //     string authenticated = (ulist[i].authenticated) ? "TRUE" : "FALSE";
+            //     dataTable.Rows.Add(new string[] { uid, email, password, logined, authenticated });
+            // }
 
-           // // DataTable의 Default View를 바인딩하기
-           //userDataGrid.ItemsSource = dataTable.DefaultView;
+            // // DataTable의 Default View를 바인딩하기
+            //userDataGrid.ItemsSource = dataTable.DefaultView;
 
         }
 
